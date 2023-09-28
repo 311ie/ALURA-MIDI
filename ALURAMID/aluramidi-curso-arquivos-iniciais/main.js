9be360cafeb07d822af3884b7ed70f84912508de
ellie <106811952+311ie@users.noreply.github.com>
2023-09-28 11:38:09 -0300

    Add files via upload

---
+++ ALURAMID/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,12 @@
+function tocasom (seletorAudio) {
+    const elemento = documento.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else {
+       
+        console.log('elemento não encontrado ou seletor inválido')
+    }
+    
+}
